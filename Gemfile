ebe2eb125551cf336e8572a8f1cc37afc4cf298e
kaiamz <amz152@gmail.com>
2015-11-11 16:43:11 -0800

    Added phony_rails gem to validate phone number field. Also validated all other form fields.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'paperclip'
 
 gem 'devise'
 
+gem 'phony_rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -1,2 +1,7 @@
 class ApplicationForm < ActiveRecord::Base
+  phony_normalize :phone, default_country_code: 'US'
+
+  validates_plausible_phone :phone, presence: true, with: /\A\+\d+/
+  validates :name, :email, :street_address, :city, :zip, :information, presence: true
+
 end
