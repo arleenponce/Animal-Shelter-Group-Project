3b55fd6d2b66a4f4d6a02391fb2169e89b2a163e
burkefielder <burke.fielder@gmail.com>
2015-11-12 10:15:46 -0800

    Installed and used validates_zipcode gem to validate the correct zipcode. Also edited the error message if user enters an invalid zipcode.

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'devise'
 
 gem 'phony_rails'
 
+gem 'validates_zipcode'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -2,6 +2,8 @@ class ApplicationForm < ActiveRecord::Base
   phony_normalize :phone, default_country_code: 'US'
 
   validates_plausible_phone :phone, presence: true, with: /\A\+\d+/
-  validates :name, :email, :street_address, :city, :zip, :information, presence: true
+  validates :zip, zipcode: {country_code: :us}
+
+  validates :name, :email, :street_address, :city, :information, presence: true
 
 end
