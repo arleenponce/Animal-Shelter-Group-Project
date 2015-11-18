6993095e56af99014acaae8c6a0c7398997f4ac4
Arleen Ponce <arleen.ponce@gmail.com>
2015-11-18 11:27:21 -0800

    Added ability to input multiple breeds in one form. Also, empty breed fields do not save to the database.

--- app/assets/javascripts/printbreed.js
+++ app/assets/javascripts/printbreed.js
@@ -1,12 +1,17 @@
-	var counter = 0;
+	var counter = 1;
 	var new_id = "breed_" + counter;
 
 	function list_breed() {
+		if (counter === 4) {
+			document.getElementById("breed_btn").disabled = true;
+		}
+
 		var div = document.getElementById("breed_output");		
 		var input = document.createElement("input");
-		input.id = "addtl_breeds"
+		input.className = "addtl_breeds"
 		input.name = new_id
 		input.type = "text"
+		input.id = new_id
 
 		var input_breed = document.getElementById('breed_name').value;
 		var text_breed = document.createTextNode(input_breed);
@@ -21,5 +26,3 @@
 		counter++;
 		new_id = "breed_" + counter;
 	};
-
-	//Rather than this approach, make dynamic "Add Another Breed" button that allows the admin to add another breed field as needed. Also consider adding "Remove" function to each field so no blanks are left. Name must be unique - need var that tracks how many field are being used.
