c8615eebbf83d17f7993454f6b78c31101ca5372
Emma Castor <emmacastor@gmail.com>
2015-12-18 14:35:34 -0800

    Updated seed file with default admin and README

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = Admin.create!(
+:email => "admin@shelterfriends.com", 
+:password => "adoptapet",
+:password_confirmation => "adoptapet")
